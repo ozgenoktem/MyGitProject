6beb33b5d86ea04a28344650463c0727466ee045
ozgenoktem <oktem.ozgen@gmail.com>
2018-03-09 23:47:20 -0500

    InitialCommit

---
+++ src/FirstGitHub/HelloClass.java
@@ -0,0 +1,16 @@
+package FirstGitHub;
+
+public class HelloClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Hello World");
+		
+		
+		
+		
+
+	}
+
+}
